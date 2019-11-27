73ed898fa547de6c19cf55bb43c79f3c62e78595
Jiří Málek <jiri.malek@gmail.com>
2019-11-27 16:47:00 +0100

    Tests in 2 different versions of bash.

--- tests.sh
+++ tests.sh
@@ -1,3 +1,26 @@
 #! /usr/bin/env bash
 
+while [[ ${#} -ne 0 ]] && [[ "${1}" != "" ]]; do
+    param="${1}"
+
+    case "${param}" in
+        "--install-deps" )
+            scversion="stable"
+
+            wget -qO- "https://storage.googleapis.com/shellcheck/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+            cp "shellcheck-${scversion}/shellcheck" /usr/bin/
+
+            shellcheck --version
+        ;;
+
+        * )
+            >&2 echo
+            >&2 echo "Unknown param '${param}'."
+
+            exit 1
+    esac
+
+    shift
+done
+
 shellcheck -x -s bash libs/* tpcp
